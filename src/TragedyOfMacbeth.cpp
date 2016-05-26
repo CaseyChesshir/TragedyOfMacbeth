cd50caa8bfbe0d3a5b314e5d96346b2d54fd72cd
CaseyChesshir <chesshir@uoregon.edu>
2016-05-26 10:19:20 -0700

    almost done

--- src/TragedyOfMacbeth.cpp
+++ src/TragedyOfMacbeth.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <typeinfo>
+#include <string.h>
 #include "Character.hpp"
 #include "json/json.h"
 #include "TragedyOfMacbeth.hpp"
-#include "Act.hpp"
-#include "Act1.hpp"
 
 using namespace std;
 
@@ -34,74 +33,41 @@ int main(){
 	}
 
 
-	cout << "root[0]: " << root[0] << endl;
-	cout << "root[1]: " << root[1] << endl;
-
 	const Json::Value &val = root[0];
-
-
-//	string wow = root[0].toStyledString();
-
-//	cout << "wow: " << wow << endl;
-
-
-	cout << "omg: " << root[0].get("play_name", "error") << endl;
 
 	int i=0;
 
-
-	vector<string> EntirePlayLines;
+	vector<pair<string,string>> EntirePlayLines;
 
 	for (Json::ValueConstIterator it = root.begin(); it != root.end(); ++it){
-		advance(it,55779);
 		const Json::Value &temp = *it;
-		cout << temp.get("text_entry","error") << endl;
 		if(temp.get("play_name", "error").compare("macbeth") == 0){
-			cout << temp.get("line_id","error") << endl; 
-			exit(0);
-			cout << temp.get("text_entry","error") << endl;
-			EntirePlayLines.push_back(temp.get("text_entry","error").asString());
+			if (temp.get("speaker","error").compare("ADRIANO DE ARMADO") != 0){
+				cout << temp.get("line_id","error") << endl; 
+			}
+			pair<string,string> currentpair = make_pair(temp.get("speaker","error").asString(),
+														temp.get("text_entry","error").asString());
+			EntirePlayLines.push_back(currentpair);
 		}
 		i++;
 	}
 
-	Act1 act1;
-	act1.Scene1();
-	act1.Scene2();
-	act1.Scene3();
-	act1.Scene4();
-	act1.Scene5();
-	act1.Scene6();
-	act1.Scene7();
+	string currentspeaker = "";
 
-	Act2 act2;
-	act2.Scene1();
-	act2.Scene2();
-	act2.Scene3();
-	act2.Scene4();
+	for(vector<pair<string,string>>::const_iterator i = EntirePlayLines.begin(); i != EntirePlayLines.end(); ++i){
+		if ((*i).first.compare(currentspeaker) != 0){
+			cout << endl << (*i).first << ": ";
+			currentspeaker = (*i).first;
+		}
+		int len = currentspeaker.length();
+		
 
-	Act3 act3;
-	act3.Scene1();
-	act3.Scene2();
-	act3.Scene3();
-	act3.Scene4();
-	act3.Scene5();
-	act3.Scene6();
+		cout << "  " << (*i).second << endl;
+	}
 
-	Act4 act4;
-	act4.Scene1();
-	act4.Scene2();
-	act4.Scene3();
+	cout << endl;
 
-	Act5 act5;
-	act5.Scene1();
-	act5.Scene2();
-	act5.Scene3();
-	act5.Scene4();
-	act5.Scene5();
-	act5.Scene6();
-	act5.Scene7();
-	act5.Scene8();
+
 
 
 	cout << "about to return" << endl;
--- src/TragedyOfMacbeth.hpp
+++ src/TragedyOfMacbeth.hpp
@@ -1,11 +1,5 @@
 #ifndef TRAGEDYOFMACBETH_HPP_ 
 #define TRAGEDYOFMACBETH_HPP_
-#include "Act.hpp"
-#include "Act1.hpp"
-#include "Act2.hpp"
-#include "Act3.hpp"
-#include "Act4.hpp"
-#include "Act5.hpp"
 class TragedyOfMacbeth{
 public:
 	vector<string> EntirePlayLines;
